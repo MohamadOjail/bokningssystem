3ae7b730e5b47a9761e47b59b7c145f3b3bfbbec
Patrik424 <crazy_train1@hotmail.com>
2022-05-18 08:46:10 +0200

    fixat lite i scenebuilder

--- src/main/java/se/ya/bokningssystem/frontend/admin/resource/ResourceController.java
+++ src/main/java/se/ya/bokningssystem/frontend/admin/resource/ResourceController.java
@@ -1,4 +1,38 @@
 package se.ya.bokningssystem.frontend.admin.resource;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class ResourceController {
+    @FXML
+    private Button btn_add_resource, btn_delete_resource, btn_edit_resource, btn_update_resource;
+
+    @FXML
+    private ListView<?> lv_resourcelist;
+
+    @FXML
+    private TextField tf_artnr, tf_description, tf_description_resource, tf_find_resource;
+
+    @FXML
+    void addResource(MouseEvent event) {
+
+    }
+
+    @FXML
+    void delete_resource(MouseEvent event) {
+
+    }
+
+    @FXML
+    void edit_resource(MouseEvent event) {
+
+    }
+
+    @FXML
+    void update_resource(MouseEvent event) {
+
+    }
 }
