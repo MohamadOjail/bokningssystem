8087e8d4476c832fbde0161b0f7f76e6994c18d6
Mohamad Ojail <mohamad.ojail@yaelev.se>
2022-05-19 16:24:39 +0200

    resolve conflict 4

--- src/main/java/se/ya/bokningssystem/MainConsole.java
+++ src/main/java/se/ya/bokningssystem/MainConsole.java
@@ -3,25 +3,5 @@ package se.ya.bokningssystem;
 public class MainConsole {
     public static void main(String[] args) {
 
-//        DummyData.generateUsers();
-//        DummyData.generateResources();
-//        DummyData.generateBookings();
-//
-//        ReportDAO reportDAO = new ReportDAO();
-//        ReportEO reportEO = new ReportEO();
-//        reportEO.setBookingIds(3L);
-//        ReportEO add = reportDAO.add(reportEO);
-//        System.out.println(add.getBookingIds());
-
-
-//        UserDAO userDAO = new UserDAO();
-////        userDAO.delete(1L);
-//        UserEO userEO = userDAO.getById(3L);
-//        Notify notify = new Notify(userEO);
-//
-//        for (Notification notification : notify.getNotifications()){
-//            System.out.println(notification);
-//        }
-
     }
 }
