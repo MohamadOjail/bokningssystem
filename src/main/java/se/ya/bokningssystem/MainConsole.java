7a7766e8f559e76f158732e0c174c4269c68e5cc
Mohamad Ojail <mohamad.ojail@yaelev.se>
2022-05-19 16:22:33 +0200

    resolve conflict 2

--- src/main/java/se/ya/bokningssystem/MainConsole.java
+++ src/main/java/se/ya/bokningssystem/MainConsole.java
@@ -1,13 +1,5 @@
 package se.ya.bokningssystem;
 
-import se.ya.bokningssystem.backend.dao.ReportDAO;
-import se.ya.bokningssystem.backend.dao.UserDAO;
-import se.ya.bokningssystem.backend.entity.Notification;
-import se.ya.bokningssystem.backend.entity.ReportEO;
-import se.ya.bokningssystem.backend.entity.UserEO;
-import se.ya.bokningssystem.backend.entity.enums.UserNamedQueries;
-import se.ya.bokningssystem.backend.util.Notify;
-
 public class MainConsole {
     public static void main(String[] args) {
 
@@ -22,14 +14,14 @@ public class MainConsole {
 //        System.out.println(add.getBookingIds());
 
 
-        UserDAO userDAO = new UserDAO();
-//        userDAO.delete(1L);
-        UserEO userEO = userDAO.getById(3L);
-        Notify notify = new Notify(userEO);
-
-        for (Notification notification : notify.getNotifications()){
-            System.out.println(notification);
-        }
+//        UserDAO userDAO = new UserDAO();
+////        userDAO.delete(1L);
+//        UserEO userEO = userDAO.getById(3L);
+//        Notify notify = new Notify(userEO);
+//
+//        for (Notification notification : notify.getNotifications()){
+//            System.out.println(notification);
+//        }
 
     }
 }
