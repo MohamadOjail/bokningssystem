e95ca5cc92f1039f3f0bcddedc99456d9bd958a8
Mohamad Ojail <mohamad.ojail@yaelev.se>
2022-05-25 09:56:30 +0200

    admin -> booking + User ready for review

--- src/main/java/se/ya/bokningssystem/MainConsole.java
+++ src/main/java/se/ya/bokningssystem/MainConsole.java
@@ -7,6 +7,9 @@ import se.ya.bokningssystem.backend.entity.enums.ResourceNamedQueries;
 public class MainConsole {
     public static void main(String[] args) {
 
+        DummyData.generateUsers();
+        DummyData.generateResources();
+        DummyData.generateBookings();
 
     }
 }
