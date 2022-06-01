99901eb1f330301a362e7f233d7a2d10cb6cc6b0
Patrik424 <crazy_train1@hotmail.com>
2022-06-01 09:21:45 +0200

    final touch again

--- src/main/java/se/ya/bokningssystem/MainConsole.java
+++ src/main/java/se/ya/bokningssystem/MainConsole.java
@@ -1,14 +1,11 @@
 package se.ya.bokningssystem;
 
-import se.ya.bokningssystem.backend.dao.ResourceDAO;
-import se.ya.bokningssystem.backend.entity.ResourceEO;
-import se.ya.bokningssystem.backend.util.BookingService;
 import se.ya.bokningssystem.backend.util.ResourceService;
 
 public class MainConsole {
     public static void main(String[] args) {
 
-        ResourceService resourceService = new ResourceService();
-        resourceService.refreshAllResource();
+       DummyData.generateUsers();
+       DummyData.generateResources();
     }
 }
